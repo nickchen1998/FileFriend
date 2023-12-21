3f1ba6bce943509b2b2f36642b3df9d87e90de9f
nick <nickchen1998@gmail.com>
2023-12-22 00:59:52 +0800

    特性: 更新檔案管理功能和配置
    - 在`.gitignore`檔案中忽略`files/`目錄
    - 在`env_settings.py`中更新`model_config`的初始化
    - 刪除`home.py`中的不必要程式碼
    - 在`home.py`中新增檔案上傳功能
    - 在`home.py`中新增檔案刪除功能
    - 在`home.py`中新增熱門搜尋的區塊
    - 在`pyproject.toml`檔案中加入`st-pages`套件
    - 在`templates`目錄中新增一個新的檔案
    
    Signed-off-by: nick <nickchen1998@gmail.com>

--- env_settings.py
+++ env_settings.py
@@ -1,7 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).parent
 
 
 class EnvSettings(BaseSettings):
     DB_NAME: str = "file_friend"
 
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
--- home.py
+++ home.py
@@ -1,35 +1,66 @@
+import os.path
 import streamlit as st
-from env_settings import EnvSettings
+from env_settings import BASE_DIR
 from utils import get_authenticator
 
-env_settings = EnvSettings()
+st.set_page_config(
+    page_title='檔案好朋友',
+    page_icon='📖'
+)
 authenticator = get_authenticator()
 name, authentication_status, username = authenticator.login(
     'Login', 'main')
 
 if authentication_status:
+    show_upload_block = False
+    show_delete_block = False
+
     with st.sidebar:
-        st.write(f'Welcome *{name}*')
+        st.title('📖 檔案好朋友 📖')
+        st.write(f'*{name}* 您好')
+        if label_name := st.text_input(label='輸入標籤查詢：'):
+            print(label_name)
+        if file_name := st.text_input(label='輸入檔案名稱查詢：'):
+            print(file_name)
+
+        superuser_list = ['root', 'nick', 'sam']
+        if username in superuser_list:
+            st.divider()
+            st.write(f"下方為管理者專區：")
+            show_upload_block = st.checkbox('顯示上傳檔案區塊')
+            show_delete_block = st.checkbox('顯示刪除檔案區塊')
+
         authenticator.logout('Logout', 'main')
 
-    st.title('Some content')
-    with st.expander("See explanation"):
-        st.write(
-            """
-        The chart above shows some numbers I picked for you.
-            I rolled actual dice for these, so they're *guaranteed* to
-            be random."""
-        )
-        st.image("https://static.streamlit.io/examples/dice.jpg")
+    if show_upload_block:
+        st.header('檔案上傳')
+        uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+        if not os.path.exists(BASE_DIR / 'files'):
+            os.mkdir(BASE_DIR / 'files')
 
-    with st.expander("See explanation"):
-        st.write(
-            """
-        The chart above shows some numbers I picked for you.
-            I rolled actual dice for these, so they're *guaranteed* to
-            be random."""
-        )
-        st.image("https://static.streamlit.io/examples/dice.jpg")
+        for uploaded_file in uploaded_files:
+            bytes_data = uploaded_file.read()
+            with open(BASE_DIR / 'files' / f'{uploaded_file.name}', 'wb') as file:
+                file.write(bytes_data)
+
+        st.divider()
+
+    if show_delete_block:
+        st.header('檔案刪除')
+
+        st.divider()
+
+    st.header('熱門搜尋')
+    for i in range(10):
+        with st.expander("See explanation"):
+            st.write(
+                """
+            The chart above shows some numbers I picked for you.
+                I rolled actual dice for these, so they're *guaranteed* to
+                be random."""
+            )
+            binary_contents = b'example content'
+            st.download_button('Download File', binary_contents, key=i)
 
 elif authentication_status is False:
     st.error('Username/password is incorrect')
