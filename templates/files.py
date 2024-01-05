cceddd7c581e30f38f0e184f555a5d25f5479168
nick <nickchen1998@gmail.com>
2024-01-05 22:40:20 +0800

    功能：重構使用者管理並刪除的功能
    - 修改設定`description`的行，改用`text_area`而非`text_input`
    - 增加檔案上傳後顯示的成功訊息
    - 更新`show_delete_block_method`函式，在刪除操作後顯示成功訊息
    - 在`show_user_manage_block_method`函式中新增一個用於使用者刪除的標籤
    - 在`show_user_manage_block_method`函式中實現按使用者名稱或電子郵件刪除使用者的邏輯
    
    Signed-off-by: nick <nickchen1998@gmail.com>

--- templates/files.py
+++ templates/files.py
@@ -11,7 +11,7 @@ def show_upload_block_method():
     with st.form(key='upload_file_form', clear_on_submit=True):
         now = datetime.now()
         tag_string = st.text_input("請輸入標籤：", placeholder='多個標籤請用半形逗號隔開')
-        description = st.text_input("請輸入檔案描述：")
+        description = st.text_area(label="請輸入檔案描述：")
         upload_file = st.file_uploader("請選擇要上傳的檔案", accept_multiple_files=False)
 
         if st.form_submit_button('上傳'):
@@ -34,6 +34,7 @@ def show_upload_block_method():
                         if file_obj:
                             with open(BASE_DIR / 'files' / f'{upload_file.name}', 'wb') as file:
                                 file.write(bytes_data)
+                            st.success("上傳成功")
 
 
 def show_delete_block_method():
@@ -51,10 +52,13 @@ def show_delete_block_method():
                     result = cruds.delete_files_by_tag(session=session, tag=tag)
                     if result is False:
                         st.error("標籤不存在")
+                    else:
+                        st.success("刪除成功")
 
             elif file_name:
                 with session_scope() as session:
                     result = cruds.delete_file_by_filename(session=session, filename=file_name)
                     if result is False:
                         st.error("檔案不存在")
-
+                    else:
+                        st.success("刪除成功")
--- templates/users.py
+++ templates/users.py
@@ -1,7 +1,76 @@
+import yaml
 import streamlit as st
+from env_settings import BASE_DIR
+from streamlit_authenticator import Hasher
 
 
 def show_user_manage_block_method():
     st.header('使用者管理')
 
-    st.divider()
+    add_user_tab, delete_user_tab = st.tabs(["新增", "刪除"])
+
+    with add_user_tab:
+        with st.form(key='add_user_form', clear_on_submit=True):
+            username = st.text_input("請輸入 username：")
+            email = st.text_input("請輸入 email：")
+            password = st.text_input("請輸入 password：", type='password')
+            password_confirm = st.text_input("請再次輸入 password：", type='password')
+
+            if st.form_submit_button('新增'):
+                if password == password_confirm:
+                    with open(BASE_DIR / 'credentials.yaml', 'r') as file:
+                        data = yaml.safe_load(file)
+
+                    if data['credentials']['usernames'].get(username):
+                        st.error("username 已存在")
+                    else:
+                        data['credentials']['usernames'][username] = {
+                            "email": email,
+                            "name": username,
+                            "password": Hasher([password]).generate()[0]
+                        }
+                        with open(BASE_DIR / 'credentials.yaml', 'w') as file:
+                            yaml.safe_dump(data, file)
+                        st.success("新增成功")
+                else:
+                    st.error("兩次 password 不一致")
+
+    with delete_user_tab:
+        with st.form(key='delete_user_form', clear_on_submit=True):
+            username = st.text_input("請輸入 username：")
+            email = st.text_input("請輸入 email：")
+
+            if st.form_submit_button('刪除'):
+                if username and email:
+                    st.error("請選擇一種刪除方式")
+                elif username:
+                    with open(BASE_DIR / 'credentials.yaml', 'r') as file:
+                        data = yaml.safe_load(file)
+
+                    if data['credentials']['usernames'].get(username):
+                        if data['credentials']['usernames'][username]['name'] == 'root':
+                            st.error("root 無法刪除")
+                        else:
+                            del data['credentials']['usernames'][username]
+                            with open(BASE_DIR / 'credentials.yaml', 'w') as file:
+                                yaml.safe_dump(data, file)
+                            st.success("刪除成功")
+                    else:
+                        st.error(f"{username} 不存在")
+                elif email:
+                    with open(BASE_DIR / 'credentials.yaml', 'r') as file:
+                        data = yaml.safe_load(file)
+
+                    for username, info in data['credentials']['usernames'].items():
+                        if info['email'] == email:
+                            if data['credentials']['usernames'][username]['name'] == 'root':
+                                st.error("root 無法刪除")
+                                break
+                            else:
+                                del data['credentials']['usernames'][username]
+                                with open(BASE_DIR / 'credentials.yaml', 'w') as file:
+                                    yaml.safe_dump(data, file)
+                                st.success("刪除成功")
+                                break
+                    else:
+                        st.error(f"{email} 不存在")
